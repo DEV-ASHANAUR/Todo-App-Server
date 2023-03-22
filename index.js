9c5f75c846b6d3cbbdac5e4d28455caf0b9e9b83
dev-ashanaur <devashanaur009@gmail.com>
2023-03-22 13:57:32 +0600

    fix error

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 const { MongoClient } = require("mongodb");
-const en = require('dotenv').config()
+const env = require('dotenv').config()
 const ObjectId = require('mongodb').ObjectId;
 
 //middleware
@@ -13,7 +13,7 @@ app.use(express.json());
 //variable
 const port = process.env.PORT || 5000;
 // Connection URI
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.88ouu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.88ouu.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 async function run() {
